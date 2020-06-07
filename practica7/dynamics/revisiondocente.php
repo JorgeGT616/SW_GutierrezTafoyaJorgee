975a4980311d517defe2fbca8a2881003f1507a4
JorgeGT616 <jorgegut616@comunidad.unam.mx>
2020-06-07 12:41:12 -0500

    FEAT: Terminada practica 7 para revision de contrasena

--- practica7/dynamics/revisiondocente.php
+++ practica7/dynamics/revisiondocente.php
@@ -10,12 +10,69 @@
     $elcolegio = (isset($_POST['colegio']) && $_POST['colegio'] )!= "" ? $_POST['colegio'] : 'Coloque su rfc' ;
     $hola="hola";
     $imposible = false;
+
+    //Se establecen arreglos con múltiples caracteres
+    $supremo = array(",","A","B","C","D","E","F","G","H","I","J","K","L",
+      "M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+      "a","b","c","d","e","f","g","h","i","j","k","l",
+      "m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z",
+      "1","2","3","4","5","6","7","8","9","0",
+      "!","@","#","$","%","&","/","(",")","=","?","¿",
+      "¡","<",">","´","¨","[","]","{","}","+","*","-",".",'"',"'");
+    //Se establece un nuevo arreglo con comas
+    $supremocoma = array(",,","A,","B,","C,","D,","E,","F,","G,","H,","I,","J,","K,","L,",
+      "M,","N,","Ñ,","O,","P,","Q,","R,","S,","T,","U,","V,","W,","X,","Y,","Z,",
+      "a,","b,","c,","d,","e,","f,","g,","h,","i,","j,","k,","l,",
+      "m,","n,","ñ,","o,","p,","q,","r,","s,","t,","u,","v,","w,","x,","y,","z,",
+      "1,","2,","3,","4,","5,","6,","7,","8,","9,","0,",
+      "!,","@,","#,","$,","%,","&,","/,","(,","),","=,","?,","¿,",
+      "¡,","<,",">,","´,","¨,","[,","],","{,","},","+,","*,","-,",".,",'",',"',");
+    //Arreglo con letras mayúsculas
+    $mayus = array("A","B","C","D","E","F","G","H","I","J","K","L",
+      "M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+    //Arreglo de caracteres sin comas
+    $sincomas = array("A","B","C","D","E","F","G","H","I","J","K","L",
+      "M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+      "a","b","c","d","e","f","g","h","i","j","k","l",
+      "m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","á","é",
+      "í","ó","ú","ä","ë","ï","ö","ü","Á","É","Í","Ó","Ú","Ä","Ë","Ï","Ö","Ü",
+      "1","2","3","4","5","6","7","8","9","0");
+    //Arreglo de mayúsculas con comas
+    $mayusconcomas = array("A,","B,","C,","D,","E,","F,","G,","H,","I,","J,","K,","L,",
+      "M,","N,","N,","O,","P,","Q,","R,","S,","T,","U,","V,","W,","X,","Y,","Z,",
+      "A,","B,","C,","D,","E,","F,","G,","H,","I,","J,","K,","L,",
+      "M,","N,","N,","O,","P,","Q,","R,","S,","T,","U,","V,","W,","X,","Y,","Z,","A,","E,",
+      "I,","O,","U,","A,","E","I,","O,","U,","A","E","I","O","U","A","E","I","O","U",
+      "1,","2,","3,","4,","5,","6,","7,","8,","9,","0,");
+    //Arreglo con letras minúsculas
+    $minus = array("a","b","c","d","e","f","g","h","i","j","k","l",
+      "m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z");
+    //Arreglo con números
+    $numeros = array("1","2","3","4","5","6","7","8","9","0");
+    //Arreglo con caracteres especiales
+    $especiales = array("!","@","#","$","%","&","/","(",")","=","?","¿",
+      "¡","<",">","´","¨","[","]","{","}","+","*","-",",",".",'"',"'");
     //Se valida que todos los datos existan
     if (($contrasenaprin === '')||($elpapellido === '')||($elmapellido === '')||($elnacimiento === '')||($elrfc === '')||($elcolegio === ''))
     {
       header('Location: ../templates/formulariodocente.html');
     }
-
+    //Verificación de que el RFC sea correcto
+    $arrPat = explode(",",str_replace($sincomas,$mayusconcomas,strtoupper($elpapellido)));
+    $arrMat = explode(",",str_replace($sincomas,$mayusconcomas,strtoupper($elmapellido)));
+    $arrNom = explode(",",str_replace($sincomas,$mayusconcomas,strtoupper($elnombre)));
+    $elNac1 = str_replace($sincomas,$mayusconcomas,$elnacimiento);
+    $arrNac2 = explode(",",str_replace("-","",$elNac1));
+    $arrRFC = explode(",",str_replace($sincomas,$mayusconcomas,strtoupper($elrfc)));
+    $rfcTemp = $arrPat[0].$arrPat[1].$arrMat[0].$arrNom[0].$arrNac2[2].$arrNac2[3].$arrNac2[4].$arrNac2[5].$arrNac2[6].$arrNac2[7];
+    $rfcComp = $arrRFC[0].$arrRFC[1].$arrRFC[2].$arrRFC[3].$arrRFC[4].$arrRFC[5].$arrRFC[6].$arrRFC[7].$arrRFC[8].$arrRFC[9];
+    if($rfcTemp != $rfcComp)
+    {
+      echo
+      "El RFC que introdujo es incorrecto, se le solicita revisar sus datos y la escritura
+      <br><a href = '../templates/formulariodocente.html'>Regresar</a>";
+      $imposible = true;
+    }
     //Filtro 1
     //Este arreglo contiene contraseñas especialmente comunes y por lo tanto vulnerables
     $contcomunes = array("1234567890","0987654321","qwertyuiop","asdfghjklñ","zxcvbnm",
@@ -25,8 +82,10 @@
       "999999","000000","qwerty","asdfgh","zxcvbn","sunshine","Sunshine", "iloveyou","ILoveYou","princess","Princess",
       "welcome","Welcome","football","Football","monkey","Monkey","abc123","abcdefg","123123","master","Master"
     );
-
-    echo $contrasenaprin;
+    if($imposible == false)
+    {
+      echo "<br>".$contrasenaprin;
+    }
     //Se compara la contraseña puesta con las contraseñas vulnerables y se evita que esta sea puesta
     foreach ($contcomunes as $pass1)
     {
@@ -60,33 +119,7 @@
 
     if($imposible == false)
     {
-      //Se establecen arreglos con múltiples caracteres
-      $supremo = array(",","A","B","C","D","E","F","G","H","I","J","K","L",
-        "M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-        "a","b","c","d","e","f","g","h","i","j","k","l",
-        "m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z",
-        "1","2","3","4","5","6","7","8","9","0",
-        "!","@","#","$","%","&","/","(",")","=","?","¿",
-        "¡","<",">","´","¨","[","]","{","}","+","*","-",".",'"',"'");
-      //Se establece un nuevo arreglo con comas
-      $supremocoma = array(",,","A,","B,","C,","D,","E,","F,","G,","H,","I,","J,","K,","L,",
-        "M,","N,","Ñ,","O,","P,","Q,","R,","S,","T,","U,","V,","W,","X,","Y,","Z,",
-        "a,","b,","c,","d,","e,","f,","g,","h,","i,","j,","k,","l,",
-        "m,","n,","ñ,","o,","p,","q,","r,","s,","t,","u,","v,","w,","x,","y,","z,",
-        "1,","2,","3,","4,","5,","6,","7,","8,","9,","0,",
-        "!,","@,","#,","$,","%,","&,","/,","(,","),","=,","?,","¿,",
-        "¡,","<,",">,","´,","¨,","[,","],","{,","},","+,","*,","-,",".,",'",',"',");
-      //Arreglo con letras mayúsculas
-      $mayus = array("A","B","C","D","E","F","G","H","I","J","K","L",
-        "M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-      //Arreglo con letras minúsculas
-      $minus = array("a","b","c","d","e","f","g","h","i","j","k","l",
-        "m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z");
-      //Arreglo con números
-      $numeros = array("1","2","3","4","5","6","7","8","9","0");
-      //Arreglo con caracteres especiales
-      $especiales = array("!","@","#","$","%","&","/","(",")","=","?","¿",
-        "¡","<",">","´","¨","[","]","{","}","+","*","-",",",".",'"',"'");
+
       $contadorperfecto = 0;
       //Se reemplazan todos los caracteres de la contraseña por unos que tienen comas
       $encomada = str_replace($supremo,$supremocoma,$contrasenaprin);
